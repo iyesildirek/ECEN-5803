7e339539483ab4079657a8544ec4eb8356156a02
iyesildirek <26639277+iyesildirek@users.noreply.github.com>
2019-01-22 08:09:33 -0600

    New file

--- Project 1/Module 2/main.cpp
+++ Project 1/Module 2/main.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright (C) 2018 by David James & Ismail Yesildirek
+* Copyright (C) 2018
 *
 * Redistribution, modification or use of this software in source or binary
 * forms is permitted as long as the files maintain this copyright. Users are
@@ -8,23 +8,27 @@
 * any misuse of this material.
 *
 *****************************************************************************/
-/**
-* @file Main.cpp
-* @brief This source file contains a c program that reads raw analog data
-* out of the built in accelerometer in the FRDM-KL25Z board. Based on the  
-* x, y, and z position the built in RGB LED will change color. This project
-* also utilizes the built in capacitive touch slider to control the RGB
-*  LED dimmability. 
-*
-* @author David James & Ismail Yesildirek
-* @date September 27 2018
-* @version 1.0
-*
-*/
+/// @file Main.cpp
+/// @brief This source file contains a c program that reads raw analog data
+/// out of the built in accelerometer in the FRDM-KL25Z board. Based on the  
+/// x, y, and z position the built in RGB LED will change color. This project
+/// also utilizes the built in capacitive touch slider to control the RGB
+///  LED dimmability. 
+///
+/// @author David James & Ismail Yesildirek
+/// @date September 27 2018
+/// @version 1.0
+///
+/*****************************************************************************/
 #include "mbed.h"
 #include "MMA8451Q.h"
 #include "tsi_sensor.h"
 
+
+/**
+ * @brief main()
+ *	this function contains the setup and main loop
+ */
 int main(void)
 {
 	  // instantiate accelerometer. SDA=PTE25, SCL=PTE24, i2c address=0x3A
@@ -32,14 +36,13 @@ int main(void)
 	  // instantiate touch slider (pins 9&10, adc range 0-40 --> 0.0f-1.0f)
 	  TSIAnalogSlider tsi(9, 10, 40);
 	  // set up PWM to LED pins
-    PwmOut rled(LED1);
-    PwmOut gled(LED2);
-    PwmOut bled(LED3);
-	  // create variable t to hold touch slider percentage
-	  float t;
-
-/*@brief Read touch slider and write to LED based on position*/	
-    while (true)
+    PwmOut rled(LED1); //!< PWM output for RED LED
+    PwmOut gled(LED2); //!< PWM output for GREEN LED
+    PwmOut bled(LED3); //!< PWM output for BLUE LED
+	  
+	  float t; //!< Holds touch slider percentage
+/* @brief Read touch slider and write to LED based on position */	
+    while (1)
 		{
 			  // read touch slider percentage
 			  t = tsi.readPercentage();	
@@ -49,5 +52,6 @@ int main(void)
         bled = t + abs(acc.getAccX());
 			  // 10 Hz update rate
         wait(0.1f);
+			  i++;
     }
 }
